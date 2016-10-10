0ba0af15d88c514386de6aa5f619b46cc4e73c19
yihong@ <841058801@qq.com>
2016-10-10 15:24:58 +0800

    修改服务器mysql地址

--- Application/Common/Conf/pro/config.php
+++ Application/Common/Conf/pro/config.php
@@ -13,10 +13,10 @@ return array(
                                 
     // '配置项'=>'配置值'
     'DB_TYPE' => 'mysql',
-    'DB_HOST' => '127.0.0.1',
+    'DB_HOST' => 'rm-bp1g3hc51n48v0fyu.mysql.rds.aliyuncs.com',
 	'DB_NAME' => 'waifood',
 	'DB_USER' => 'waifood',
-	'DB_PWD' => 'ek2bzd82',
+	'DB_PWD' => 'Waifood2016',
     'DB_PORT' => '3306',
     'SHOW_ERROR_MSG' => true,
     'DB_PREFIX' => 'my_',
