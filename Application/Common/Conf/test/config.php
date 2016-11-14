b7a79766aa28f555976bf9754c1526e79489f56f
yihong@ <841058801@qq.com>
2016-11-14 15:40:19 +0800

    数据库配置

--- Application/Common/Conf/test/config.php
+++ Application/Common/Conf/test/config.php
@@ -2,10 +2,10 @@
 return array(
     // '配置项'=>'配置值'
     'DB_TYPE' => 'mysql',
-    'DB_HOST' => 'rm-bp1g3hc51n48v0fyu.mysql.rds.aliyuncs.com',
+    'DB_HOST' => '127.0.0.1',
     'DB_NAME' => 'waifood',
-    'DB_USER' => 'waifood',
-    'DB_PWD' => 'Waifood2016',
+    'DB_USER' => 'root',
+    'DB_PWD' => 'root',
     'DB_PORT' => '3306',
     'SHOW_ERROR_MSG' => true,
     'DB_PREFIX' => 'my_',
