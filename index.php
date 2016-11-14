4098c02d457bdc0d11a79440719a5b235deb875d
yihong@ <841058801@qq.com>
2016-11-14 13:07:42 +0800

    测试服务

--- index.php
+++ index.php
@@ -3,6 +3,9 @@ $hostname = php_uname('n');
 if (strtolower($hostname) != 'pc-20160819tbmr') {
     define('DEPLOY_ENV', 'pro'); // 线上模式
     define('HIDDEN_ENV_FLAG', false); // 环境标示
+}elseif(strtolower($hostname) != 'izuf62o440hd7lymnaldnwz'){
+    define('DEPLOY_ENV', 'test'); // 本地模式
+    define('HIDDEN_ENV_FLAG', true); // 环境标示显示
 } else {
     define('DEPLOY_ENV', 'local'); // 本地模式
     define('HIDDEN_ENV_FLAG', true); // 环境标示显示
