ab79dcb7bfddfbc41e6363e4b939e21208d24b84
yihong@ <841058801@qq.com>
2016-11-15 09:08:07 +0800

    test conf

--- Application/Common/Conf/test/config.php
+++ Application/Common/Conf/test/config.php
@@ -2,8 +2,8 @@
 return array(
     // '配置项'=>'配置值'
     'DB_TYPE' => 'mysql',
-    'DB_HOST' => '127.0.0.1',
-    'DB_NAME' => 'waifood',
+    'DB_HOST' => '172.16.23.209',
+    'DB_NAME' => 'waifood_test',
     'DB_USER' => 'root',
     'DB_PWD' => 'root',
     'DB_PORT' => '3306',
