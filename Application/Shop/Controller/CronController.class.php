dd2c074527866d24970f4b9fd8c678e60a87c72a
yihong@ <841058801@qq.com>
2017-01-18 17:48:06 +0800

    ddd

--- Application/Shop/Controller/CronController.class.php
+++ Application/Shop/Controller/CronController.class.php
@@ -11,13 +11,11 @@ class CronController extends Controller
 {
         
     public function getProductSold(){
-//        ProductSoldModel::getProductAVGSoldByDay(7);
-        ProductSoldModel::getProductAVGSoldByDay(30);
-        exit;
         $time = date('Y-m-d',strtotime("-1 day"));
         //获取前一天上商品销售量
         if(false!==ProductSoldModel::getProductDaySold($time)){
-
+            ProductSoldModel::getProductAVGSoldByDay(7);
+            ProductSoldModel::getProductAVGSoldByDay(30);
         }
     }
 
