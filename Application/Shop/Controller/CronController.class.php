fd305cda5d467106106864ef8bc83c3eecdc886d
yihong@ <841058801@qq.com>
2017-01-19 09:37:49 +0800

    ddd

--- Application/Shop/Controller/CronController.class.php
+++ Application/Shop/Controller/CronController.class.php
@@ -9,14 +9,19 @@ use Think\Controller;
 
 class CronController extends Controller
 {
-        
+
     public function getProductSold(){
-        $time = date('Y-m-d',strtotime("-1 day"));
-        //获取前一天上商品销售量
-        if(false!==ProductSoldModel::getProductDaySold($time)){
-            ProductSoldModel::getProductAVGSoldByDay(7);
-            ProductSoldModel::getProductAVGSoldByDay(30);
+        for($i=1;$i<40;$i++){
+            $time = date('Y-m-d',strtotime("-$i day"));
+            //获取前一天上商品销售量
+            if(false!==ProductSoldModel::getProductDaySold($time)){
+                ProductSoldModel::getProductAVGSoldByDay(7);
+                ProductSoldModel::getProductAVGSoldByDay(30);
+            }
+          //  ProductSoldModel::getProductDaySold($time);//获取前一天上商品销售量
         }
+        // ProductSoldModel::getProductAVGSoldByDay(7);
+        // $time = date('Y-m-d',strtotime("-1 day"));
     }
 
 
