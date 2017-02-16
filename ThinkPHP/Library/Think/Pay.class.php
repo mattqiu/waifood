dbfa66f6364d917ca1f2ab993a3b09bc0f07673f
yihong@ <841058801@qq.com>
2017-02-16 15:23:13 +0800

    回调连接

--- ThinkPHP/Library/Think/Pay.class.php
+++ ThinkPHP/Library/Think/Pay.class.php
@@ -66,8 +66,8 @@ class Pay {
             'out_trade_no' => $vo->getOrderNo(),
             'money' => $vo->getFee(),
             'status' => 0,
-            'callback' => $vo->getCallback(),
-            'url' => $vo->getUrl(),
+            'notify_url' => $vo->getCallback(),
+            'return_url' => $vo->getUrl(),
             'param' => serialize($vo->getParam()),
             'create_time' => time(),
             'update_time' => time()
