a113af7f4f5b02672af041499a13360ce1743c17
yihong@ <841058801@qq.com>
2017-02-23 15:24:28 +0800

    font size

--- Application/Common/Common/share.php
+++ Application/Common/Common/share.php
@@ -17,7 +17,7 @@ function isVerifyCorrect()
 
 
 function version(){
-    return 'v.2.23-1'; //日期号
+    return 'v.2.23-2'; //日期号
 }
 
 function triggerRequest($url, $post_data = array(), $cookie = array()){
